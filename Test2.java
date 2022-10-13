3bef7212a547169bb51d5a41be13d2c5f319be70
learnedsage <114851607+learnedsage@users.noreply.github.com>
2022-10-13 21:58:02 +0530

    Update Test2.java

--- Test2.java
+++ Test2.java
@@ -26,26 +26,28 @@ class Launch{
             System.out.println("invalid input");
     }
 
-String email() {
-    String b;
+    String email() {
+        String b;
 
-    System.out.print("Enter the email id");
-    Scanner scann = new Scanner(System.in);
-    b = scann.next();
-    if (b.contains("@")&& (b.endsWith(".com"))){
-        return b;
-    }
-
-            else{
-            System.out.println("invalid email-id");
-            email();
+        System.out.print("Enter the email id");
+        Scanner scann = new Scanner(System.in);
+        b = scann.next();
+       if ((b.contains("@"))&&(b.endsWith(".com"))){
             return b;
+        }
+
+        else{
+            System.out.println("invalid email-id");
+
+            String  d="**";
+            return d;
+
         }
     }
 
 }
 
-class Test2 {
+class Test1{
 
 
     public static void main(String args[]) {
@@ -55,31 +57,34 @@ class Test2 {
         Scanner scan = new Scanner(System.in);
         a = scan.next();
 
-       Launch em= new Launch();
-       String y = em.email();
-       System.out.println("1.Mobile\n2.Laptop\n3.Electonic Appliances\n4.Perfumes\n Enter the choice by number");
+        Launch em= new Launch();
+        String y = em.email();
+        for(int i =0;y=="**";i++){
+           y= em.email();
+            break;}
+        System.out.println("1.Mobile\n2.Laptop\n3.Electonic Appliances\n4.Perfumes\n Enter the choice by number");
         Scanner scannn = new Scanner(System.in);
         String[] ekart={"Mobile","laptop","electonic","perfume"};
         int c;
         for (int i=0; i<4;i++){
             c = scannn.nextInt();
-        if(c>4){
-            System.out.println("Enter a  valid input: ");}
+            if(c>4){
+                System.out.println("Enter a  valid input: ");}
             else{
 
-              Launch calc= new Launch();
-              calc.Ekart(c);
+                Launch calc= new Launch();
+                calc.Ekart(c);
 
-              break;
+                break;
 
             }
 
         }
 
-    System.out.println("Enter your choics using the number: ");
-    Scanner scnnn = new Scanner(System.in);
-    int d= scnnn.nextInt();
+        System.out.println("Enter your choics using the number: ");
+        Scanner scnnn = new Scanner(System.in);
+        int d= scnnn.nextInt();
 
-    System.out.println(a+ " thanks for purchasing \n visit again "+"\nyour bill has been sent to your email-id "+y);
+        System.out.println(a+ " thanks for purchasing \n visit again "+"\nyour bill has been sent to your email-id "+y);
 
-}}
+    }}
